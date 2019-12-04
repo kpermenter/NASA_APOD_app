0fb2f0914dcb4b297a55394f154906104059c5d7
kpermenter <52431178+kpermenter@users.noreply.github.com>
2019-12-04 14:15:35 -0600

    Add files via upload

--- fetchapi/fetch.js
+++ fetchapi/fetch.js
@@ -1,47 +1,31 @@
-const select = document.getElementById("select");
+const head = document.getElementById("title");
+const time = document.getElementById("date");
 const card = document.getElementById("card");
+const body = document.getElementById("body");
 
-fetch("https://api.nasa.gov/planetary/apod?api_key=AdxcWHpgEafWbnh3UvyHDetm27nHLofe9vRS8DHU")
-.then(response => response.json()) 
-.then(data => generateImage(data.hdurl, data.explanation))
-// .then(data => generateText(data.hdurl))
-// .then(data => console.log(data.explanation))
+function fetchData(url) {
+    return fetch(url)
+        .then(res => res.json())
+}
 
+fetchData("https://api.nasa.gov/planetary/apod?api_key=AdxcWHpgEafWbnh3UvyHDetm27nHLofe9vRS8DHU")
+.then(data => generateContent(data.title, data.date, data.hdurl, data.explanation))
 
-//RENDER FUNCTIONS
-function generateImage(hdurl, explanation) {
+function generateContent(title, date, hdurl, explanation) {
+const titleHTML = `
+<h1>${title}</h1>
+`;
+const dateHTML = `
+<h6>${date}</h6>
+`;
 const imageHTML = `
 <img src="${hdurl}" alt>
 `;
-card.innerHTML = imageHTML;
-
 const textHTML = `
     <p>"${explanation}"</p>
     `;
-    select.innerHTML = textHTML;
+    head.innerHTML = titleHTML;
+    time.innerHTML = dateHTML;
+    card.innerHTML = imageHTML;
+    body.innerHTML = textHTML;
 }
-
-// function generateText(textData) {
-//     const textHTML = `
-//     <p>"${textData}"</p>
-//     `;
-//     select.innerHTML = textHTML;
-//     }
-
-
-// const select = document.getElementById("select");
-// const card = document.getElementById("card");
-
-// fetch("https://api.nasa.gov/planetary/apod?api_key=AdxcWHpgEafWbnh3UvyHDetm27nHLofe9vRS8DHU")
-// .then(response => response.json()) 
-// // .then(data => console.log(data.hdurl))
-// .then(data => generateImage(data.hdurl))
-
-// //HELPER FUNCTIONS
-// function generateImage(data) {
-// const html = `
-// <img src="${data}" alt>
-// <p>text</p>
-// `;
-// card.innerHTML = html;
-// }
